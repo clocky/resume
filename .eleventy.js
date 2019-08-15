bba861ba12737951292180e6507b9ffccc940098
Mark McLaughlin <mark@mclaughlin.me.uk>
2019-08-14 22:36:44 -0700

    Add pretty, alternate folders

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+module.exports = function(eleventyConfig) {
+    var pretty = require('pretty');
+  eleventyConfig.addTransform("pretty", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let prettified = pretty(content, {
+        ocd: true,
+      });
+      return prettified;
+    }
+    return content;
+  });
+  
+  return {
+        templateFormats: ["pug", "njk"],
+
+        pathPrefix: "/",
+        htmlTemplateEngine: "njk",
+        passthroughFileCopy: true,
+        dir: {
+        input: "_site",
+        includes: "_templates",
+        data: "_data",
+        output: "dist"
+        }
+    }
+}
