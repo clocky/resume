31f4191edbed9301de25ec6f097c08bd24f4d9aa
unknown <mark@mclaughlin.me.uk>
2020-01-18 15:48:00 -0800

    chore: no mo assets

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy({"src/_assets/img/": "/img"});
   eleventyConfig.addPassthroughCopy({"src/_assets/favicon/": "/"});
   eleventyConfig.addPassthroughCopy({"src/_assets/css/": "/css"});
